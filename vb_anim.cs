f512f6497d5c67e9ec6e64d374ac0ebe08be2023
RMUTP <65149622+Railway-RMUTP@users.noreply.github.com>
2020-08-21 19:32:31 +0700

    first commit

---
+++ vb_anim.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vb_anim : MonoBehaviour, IVirtualButtonEventHandler 
+{
+
+    public GameObject vbBtnObj;
+    public Animator cubeAni;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vbBtnObj = GameObject.Find("myButton");
+
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        cubeAni.GetComponent<Animator>();
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        cubeAni.Play("Cube_Animation");
+        Debug.Log("Button Pressed");
+    }
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        cubeAni.Play("none");
+        Debug.Log("Button Released");
+    }
+
+}
